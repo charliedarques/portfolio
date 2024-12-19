4abbe73150d38d9582a4dfbca1fe6ed544a0f15c
charliedarques <117463719+charliedarques@users.noreply.github.com>
2024-12-19 10:59:33 +0100

    upload files

---
+++ public/js/popupProjets.js
@@ -0,0 +1,47 @@
+function seeProject1 () {
+    projet1.classList.add("show");
+}
+function seeProject2 () {
+    projet2.classList.add("show");
+}
+function seeProject3 () {
+    projet3.classList.add("show");
+}
+function seeProject4 () {
+    projet4.classList.add("show");
+}
+function seeProject5 () {
+    projet5.classList.add("show");
+}
+function seeProject6 () {
+    projet6.classList.add("show");
+}
+
+function closeProject1 () {
+    projet1.classList.remove(
+        "show"
+    );
+}
+function closeProject2 () {
+    projet2.classList.remove(
+        "show"
+    );
+}function closeProject3 () {
+    projet3.classList.remove(
+        "show"
+    );
+}function closeProject4 () {
+    projet4.classList.remove(
+        "show"
+    );
+}function closeProject5 () {
+    projet5.classList.remove(
+        "show"
+    );
+}
+
+function closeProject6 () {
+    projet6.classList.remove(
+        "show"
+    );
+}
